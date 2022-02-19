ef320841aa400a5dfa8fb308de972115c00a568e
MINGYEOL KIM <kmg1902@postech.ac.kr>
2022-02-19 19:20:04 +0900

    Sine Anim

--- src/component/Nkmg.js
+++ src/component/Nkmg.js
@@ -1,23 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { useState } from "react";
 import { useDrag } from "@use-gesture/react";
 import { useSpring, animated } from "react-spring";
 
+const speed = 300;
+const range = 30;
+const offset = 30;
+
 function Drag_Child({ sx, sy }) {
+    const ref = useRef();
     // const props = useSpring({ opacity: 0, from: { opacity: 1 } });
-    const [style, api] = useSpring(() => ({ x: 0, y: 0 }));
+    const [style, api] = useSpring(() => ({ x: 0, y: 0, onRest() {
+        console.log('Stopped')
+      } }));
+    console.log("rerender1");
 
     useEffect(() => {
-        api.start({config: {friction:26, tension:400} });
+        function animate() {
+          // const x = Math.cos(Date.now() / speed) * range;
+          const x = 0;
+          const y = Math.sin(Date.now() / speed) * range;
+          // const z = Math.sin(Date.now() / speed) * range + offset;
+    
+          ref.current.style = `position: absolute; transform: translate3d(${x}px,${y}px,0)`;
+          requestAnimationFrame(animate);
+        }
+        animate();
+      }, [ref]);
+
+    useEffect(() => {
+        api.start({config: {friction:26, tension:360}});
     }, [])
 
     useEffect(() => {
         api.start({x:sx, y:sy});
-        console.log("setting done");
     }, [sx, sy])
+
+    console.log(style.x);
     
-    return <animated.h1 style={style}>asdf</animated.h1>;
+    return (<animated.div style={style}>
+                <h1 ref = {ref}>
+                    asdf
+                </h1>
+            </animated.div>);
     // return <h1 style={{ transform: `translate3d(${x}px,${y}px,0)` }}>asdf</h1>;
 }
 
